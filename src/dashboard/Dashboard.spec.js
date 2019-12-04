bb65b5b9ce7fdb16b3144fc676c39107b547be63
osogrizz <lhtor@gmail.com>
2019-12-03 20:58:59 -0800

    init commit.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('Should match snapshot including; Controls and Display', () => {
+    // const wrapper = rtl.
+    // expect()
+  })
+})
